85b537e8815b2257d5014f5893ea10f6594c7dd3
MCT32 <ivanhen99@gmail.com>
2020-07-07 15:10:41 +1000

    colors

--- test/test.cpp
+++ test/test.cpp
@@ -5,6 +5,35 @@
 
 float angle = 0.0f;
 
+float red = 1.0f;
+float green = 1.0f;
+float blue = 1.0f;
+
+void processKeys(unsigned char key, int x, int y)
+{
+  if (key == 27)
+    exit(0);
+}
+
+void processSpecialKeys(int key, int x, int y)
+{
+  switch (key)
+  {
+    case GLUT_KEY_F1 :
+      red = 1.0f;
+      green = 0.0f;
+      blue = 0.0f; break;
+    case GLUT_KEY_F2 :
+      red = 0.0f;
+      green = 1.0f;
+      blue = 0.0f; break;
+    case GLUT_KEY_F3 :
+      red = 0.0f;
+      green = 0.0f;
+      blue = 1.0f; break;
+  }
+}
+
 void renderScene(void)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -17,6 +46,7 @@ void renderScene(void)
 
   glRotatef(angle, 0.0f, 0.0f, 1.0f);
 
+  glColor3f(red, green, blue);
   glBegin(GL_QUADS);
     glVertex3f(-0.5f, -0.5f, 0.0f);
     glVertex3f(-0.5f,  0.5f, 0.0f);
@@ -39,6 +69,9 @@ int main(int argc, char **argv)
 
   glutDisplayFunc(renderScene);
   glutIdleFunc(renderScene);
+
+  glutKeyboardFunc(processKeys);
+  glutSpecialFunc(processSpecialKeys);
 
   glutMainLoop();
 
