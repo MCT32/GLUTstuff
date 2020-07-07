0393f4b9973114ce28a8e323c580636e6cb86fee
MCT32 <ivanhen99@gmail.com>
2020-07-07 14:51:18 +1000

    animation

--- test/test.cpp
+++ test/test.cpp
@@ -3,16 +3,28 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+float angle = 0.0f;
+
 void renderScene(void)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+  glLoadIdentity();
+
+  //gluLookAt(	0.0f, 0.0f, 10.0f,
+	//			      0.0f, 0.0f,  0.0f,
+	//			      0.0f, 1.0f,  0.0f);
+
+  glRotatef(angle, 0.0f, 0.0f, 1.0f);
+
   glBegin(GL_QUADS);
-    glVertex3f(-0.5, -0.5, 0.0);
-    glVertex3f(-0.5,  0.5, 0.0);
-    glVertex3f( 0.5,  0.5, 0.0);
-    glVertex3f( 0.5, -0.5, 0.0);
+    glVertex3f(-0.5f, -0.5f, 0.0f);
+    glVertex3f(-0.5f,  0.5f, 0.0f);
+    glVertex3f( 0.5f,  0.5f, 0.0f);
+    glVertex3f( 0.5f, -0.5f, 0.0f);
   glEnd();
+
+  angle += 0.1f;
 
   glutSwapBuffers();
 }
@@ -26,6 +38,7 @@ int main(int argc, char **argv)
 	glutCreateWindow("GLUT test");
 
   glutDisplayFunc(renderScene);
+  glutIdleFunc(renderScene);
 
   glutMainLoop();
 
